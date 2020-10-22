060e87d4b29ea0f6472e67ebd96a33e44ccbbb8a
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-10-22 15:34:17 +0900

    201022 Thur 15:33, 코드 구조 수정.

--- com_cheese_api/ext/routes.py
+++ com_cheese_api/ext/routes.py
@@ -9,21 +9,9 @@ from com_cheese_api.login.sign_up_api import SignUpAPI
 
 def initialize_routes(api):
     api.add_resource(HomeAPI, '/api')
-
-def initialize_routes_cheese(cheese_api):
-    cheese_api.add_resource(CheeseAPI, '/api/cheese')
-
-def initialize_routes_board(board_api):
-    board_api.add_resource(BoardAPI, '/api/board')
-
-def initialize_routes_suggest(suggest_api):
-    suggest_api.add_resource(SuggestAPI, '/api/suggest')
-
-def initialize_routes_admin(admin_api):
-    admin_api.add_resource(AdminAPI, '/api/admin')
-
-def initialize_routes_login(login_api):
-    login_api.add_resource(LoginAPI, '/api/login')
-
-def initialize_routes_logout(sign_up_api):
-    sign_up_api.add_resource(SignUpAPI, '/api/sign_up')
+    api.add_resource(CheeseAPI, '/api/cheese')
+    api.add_resource(BoardAPI, '/api/board')
+    api.add_resource(SuggestAPI, '/api/suggest')
+    api.add_resource(AdminAPI, '/api/admin')
+    api.add_resource(LoginAPI, '/api/login')
+    api.add_resource(SignUpAPI, '/api/sign_up')
--- main.py
+++ main.py
@@ -2,22 +2,9 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from com_cheese_api.ext.routes import initialize_routes
-from com_cheese_api.ext.routes import initialize_routes_cheese
-from com_cheese_api.ext.routes import initialize_routes_board
-from com_cheese_api.ext.routes import initialize_routes_suggest
-from com_cheese_api.ext.routes import initialize_routes_admin
-from com_cheese_api.ext.routes import initialize_routes_login
-from com_cheese_api.ext.routes import initialize_routes_logout
-
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 initialize_routes(api)
-initialize_routes_cheese(api)
-initialize_routes_board(api)
-initialize_routes_suggest(api)
-initialize_routes_admin(api)
-initialize_routes_login(api)
-initialize_routes_logout(api)
